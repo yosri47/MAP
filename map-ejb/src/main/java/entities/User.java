53beb15c81b3abb853c0e0a82a13b599aec27a63
Yosri Bedoui <yosri.bedoui@esprit.tn>
2018-10-27 22:24:28 +0100

    Fixed User entity

--- map-ejb/src/main/java/entities/User.java
+++ map-ejb/src/main/java/entities/User.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
