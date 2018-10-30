6d1cb597e4fa609ea5f8a6cb7da491c17850d036
Yosri Bedoui <yosri.bedoui@esprit.tn>
2018-10-30 18:51:03 +0100

    dd

--- map-ejb/src/main/java/entities/Ressource.java
+++ map-ejb/src/main/java/entities/Ressource.java
@@ -1,12 +1,35 @@
 package entities;
 
 import java.io.Serializable;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import javax.persistence.ManyToOne;
+
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Ressource extends User implements Serializable{
@@ -18,7 +41,7 @@ public class Ressource extends User implements Serializable{
 	private int seniority;
 	private String note;
 	private double rate;
-	private String contractType;
+	private ContractType contractType;
 	@Column(columnDefinition= "boolean default false")
 	private boolean isOnLeave;
 	@Column(columnDefinition = "boolean default true")
@@ -30,24 +53,176 @@ public class Ressource extends User implements Serializable{
 	@OneToOne
 	@JoinColumn(name="resumeId")
 	private Resume resume;
+
 	@OneToOne
 	@JoinColumn(name="mandateId")
 	private Mandate mandate;
+	
+	@OneToMany(mappedBy = "rssend", cascade = CascadeType.ALL ,fetch=FetchType.EAGER )	
+	private Set<Message>rssends ;
+   
+	@OneToMany(mappedBy = "rsrecu", cascade = CascadeType.ALL ,fetch=FetchType.EAGER )	
+	private Set<Message>recu ;
+	
+	
+
+
+	public Ressource(int userId) {
+		super(userId);
+	}
+    @JsonIgnore
+
+	public Set<Message> getRssends() {
+		return rssends;
+	}
+
+	public void setRssends(Set<Message> rssends) {
+		this.rssends = rssends;
+	}
+    @JsonIgnore
+
+	public Set<Message> getRecu() {
+		return recu;
+	}
+
+	public void setRecu(Set<Message> recu) {
+		this.recu = recu;
+	}
+
 	public Ressource() {
 		super();
 	}
 	
+	
+	@ManyToOne
+	@JoinColumn(name="projectId")
+	private Project project;
+
+
+	public String getPhoto() {
+		return photo;
+	}
+
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+
+	public AvailabilityType getAvailability() {
+		return availability;
+	}
+
+
+	public void setAvailability(AvailabilityType availability) {
+		this.availability = availability;
+	}
+
+
+	public String getSector() {
+		return sector;
+	}
+
+
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+
+
+	public int getSeniority() {
+		return seniority;
+	}
+
+
+	public void setSeniority(int seniority) {
+		this.seniority = seniority;
+	}
+
+
+	public String getNote() {
+		return note;
+	}
+
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+
+	public double getRate() {
+		return rate;
+	}
+
+
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+
+
+	public ContractType getContractType() {
+		return contractType;
+	}
+
+
+	public void setContractType(ContractType contractType) {
+		this.contractType = contractType;
+	}
+
+
+	public boolean isOnLeave() {
+		return isOnLeave;
+	}
+
+
+	public void setOnLeave(boolean isOnLeave) {
+		this.isOnLeave = isOnLeave;
+	}
+
+
 	public boolean isActive() {
 		return isActive;
 	}
+
 
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
 	}
 
+
+	public Break getLeave() {
+		return leave;
+	}
+
+
+	public void setLeave(Break leave) {
+		this.leave = leave;
+	}
+
+
+	public Resume getResume() {
+		return resume;
+	}
+
+
+	public void setResume(Resume resume) {
+		this.resume = resume;
+	}
+
+
+
+	public Project getProject() {
+		return project;
+	}
+
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+
 	public Ressource(String photo, AvailabilityType availability, String sector, int seniority, String note,
-			double rate, String contractType, boolean isOnLeave, Break leave, Resume resume,
-			Mandate mandate,boolean isActive) {
+			double rate, ContractType contractType, boolean isOnLeave, boolean isActive, Break leave, Resume resume,
+			Set<Mandate> mandates, Project project) {
 		super();
 		this.photo = photo;
 		this.availability = availability;
@@ -57,88 +232,28 @@ public class Ressource extends User implements Serializable{
 		this.rate = rate;
 		this.contractType = contractType;
 		this.isOnLeave = isOnLeave;
+		this.isActive = isActive;
 		this.leave = leave;
 		this.resume = resume;
-		this.mandate = mandate;
-		this.isActive=isActive;
+		this.project = project;
 	}
-	public String getPhoto() {
-		return photo;
-	}
-	public void setPhoto(String photo) {
-		this.photo = photo;
-	}
-	public AvailabilityType getAvailability() {
-		return availability;
-	}
-	public void setAvailability(AvailabilityType availability) {
-		this.availability = availability;
-	}
-	public String getSector() {
-		return sector;
-	}
-	public void setSector(String sector) {
-		this.sector = sector;
-	}
-	public int getSeniority() {
-		return seniority;
-	}
-	public void setSeniority(int seniority) {
-		this.seniority = seniority;
-	}
-	public String getNote() {
-		return note;
-	}
-	public void setNote(String note) {
-		this.note = note;
-	}
-	public double getRate() {
-		return rate;
-	}
-	public void setRate(double rate) {
-		this.rate = rate;
-	}
-	public String getContractType() {
-		return contractType;
-	}
-	public void setContractType(String contractType) {
-		this.contractType = contractType;
-	}
-	public boolean isOnLeave() {
-		return isOnLeave;
-	}
-	public void setOnLeave(boolean isOnLeave) {
-		this.isOnLeave = isOnLeave;
-	}
-	public Break getLeave() {
-		return leave;
-	}
-	public void setLeave(Break leave) {
-		this.leave = leave;
-	}
-	public Resume getResume() {
-		return resume;
-	}
-	public void setResume(Resume resume) {
-		this.resume = resume;
-	}
-	public Mandate getMandate() {
-		return mandate;
-	}
-	public void setMandate(Mandate mandate) {
-		this.mandate = mandate;
-	}
+
+
+	
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
 		result = prime * result + ((availability == null) ? 0 : availability.hashCode());
 		result = prime * result + ((contractType == null) ? 0 : contractType.hashCode());
+		result = prime * result + (isActive ? 1231 : 1237);
 		result = prime * result + (isOnLeave ? 1231 : 1237);
 		result = prime * result + ((leave == null) ? 0 : leave.hashCode());
-		result = prime * result + ((mandate == null) ? 0 : mandate.hashCode());
 		result = prime * result + ((note == null) ? 0 : note.hashCode());
 		result = prime * result + ((photo == null) ? 0 : photo.hashCode());
+		result = prime * result + ((project == null) ? 0 : project.hashCode());
 		long temp;
 		temp = Double.doubleToLongBits(rate);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
@@ -147,6 +262,8 @@ public class Ressource extends User implements Serializable{
 		result = prime * result + seniority;
 		return result;
 	}
+
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -163,17 +280,14 @@ public class Ressource extends User implements Serializable{
 				return false;
 		} else if (!contractType.equals(other.contractType))
 			return false;
+		if (isActive != other.isActive)
+			return false;
 		if (isOnLeave != other.isOnLeave)
 			return false;
 		if (leave == null) {
 			if (other.leave != null)
 				return false;
 		} else if (!leave.equals(other.leave))
-			return false;
-		if (mandate == null) {
-			if (other.mandate != null)
-				return false;
-		} else if (!mandate.equals(other.mandate))
 			return false;
 		if (note == null) {
 			if (other.note != null)
@@ -184,6 +298,11 @@ public class Ressource extends User implements Serializable{
 			if (other.photo != null)
 				return false;
 		} else if (!photo.equals(other.photo))
+			return false;
+		if (project == null) {
+			if (other.project != null)
+				return false;
+		} else if (!project.equals(other.project))
 			return false;
 		if (Double.doubleToLongBits(rate) != Double.doubleToLongBits(other.rate))
 			return false;
@@ -201,5 +320,15 @@ public class Ressource extends User implements Serializable{
 			return false;
 		return true;
 	}
+	public Mandate getMandate() {
+		return mandate;
+	}
+	public void setMandate(Mandate mandate) {
+		this.mandate = mandate;
+	}
+	
+	
+	
+	
 	
 }
